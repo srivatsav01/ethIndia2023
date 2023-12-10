5b768c1b5553f4a069af385c91de3f7f62dc4b81
srivatsav01 <cs20btech11003@iith.ac.in>
2023-12-10 07:48:21 +0530

    add anonAadhaar component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from 'react-router-dom';
+import { AnonAadhaarProvider } from 'anon-aadhaar-react';
+const app_id = process.env.REACT_APP_ID || '';
+console.log(app_id);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  // <React.StrictMode>
+  <AnonAadhaarProvider _appId={app_id} _testing={false}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AnonAadhaarProvider>
+  // </React.StrictMode>
+);
